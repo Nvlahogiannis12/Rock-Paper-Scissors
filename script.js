b73dbe53ae3f894fd6cd54cfd917d5a87a55ab6e
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2025-01-08 18:48:28 +0000

    popup-buttons-centered

--- script.js
+++
@@ -1,4 +0,0 @@
-function test() {
-    let mode = document.getElementsByClassName("mode")[0];
-    console.log(mode.getBoundingClientRect().left, window.innerWidth, mode.offsetWidth);
-}//24 pixels unacounted for
