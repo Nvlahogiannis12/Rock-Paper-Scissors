053d58340e4d248711e457581f1c5d6a28ecb636
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2025-01-09 10:39:55 -0500

    Menu Works (code back button)

---
+++ script.js
@@ -0,0 +1,11 @@
+
+/*classList object 
+
+myElem.classList.remove("d-none")
+myElem.classList.remove("d-flex")
+*/
+function startMenu(){
+let menu = document.getElementsByClassName('mode')[0]
+menu.classList.remove("d-none")
+menu.classList.add("d-flex")
+}
