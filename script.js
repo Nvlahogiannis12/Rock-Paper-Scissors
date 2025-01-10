32250ac552d809af779684f1825dbe3e4c797511
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2025-01-10 08:11:19 -0500

    back button works

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ let menu = document.getElementsByClassName('mode')[0]
 menu.classList.remove("d-none")
 menu.classList.add("d-flex")
 }
+
+function back(){
+let closeMenu = document.getElementsByClassName('mode')[0]
+closeMenu.classList.add("d-none")
+closeMenu.classList.remove("d-flex")
+}
