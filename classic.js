07e53b84f942ac5b8e001ba59fb33c83aaa3faa4
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2025-01-10 17:12:44 +0000

    Username works

--- classic.js
+++ classic.js
@@ -1,9 +1,10 @@
-/*
+// Generates random number if guest name Chosen
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   let randomNumber = getRandomNumber(0, 9999);
-*/
+
+//inputs username
 let userName = prompt('What is your name:')
 userName ? document.getElementById('name').innerText = `${userName}`:
-document.getElementById('name').innerText ='Guest'
+document.getElementById('name').innerText =`Guest_` + `${randomNumber}`
