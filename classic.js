c742e73467ea796312b5cb7c4230d54eb5e771e1
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2025-01-13 23:57:06 +0000

    computer choice not working (fix)

--- classic.js
+++ classic.js
@@ -8,3 +8,37 @@ function getRandomNumber(min, max) {
 let userName = prompt('What is your name:')
 userName ? document.getElementById('name').innerText = `${userName}`:
 document.getElementById('name').innerText =`Guest_` + `${randomNumber}`
+
+//opponent choice
+function opponentChoice(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+let computerNumber = opponentChoice(1, 3);
+
+// assigns opponent choice a proper choice in RPS
+if(computerNumber === 1){
+ let opponentChoice = 'rock'
+}
+else if(computerNumber === 2){
+  let opponentChoice = 'paper'
+}
+else {
+  let opponentChoice = 'scissors'
+}
+
+//If rock is chosen
+function rock(){
+let playerChoice = 'rock'
+}
+
+//if paper is chosen
+function paper(){
+let playerChoice = 'paper'
+}
+
+//if scissors is chosen
+function scissors(){
+let playerChoice = 'scissors'
+}
+
+document.getElementById('results').innerText =`opponent choice: ` + `${opponentChoice}`
