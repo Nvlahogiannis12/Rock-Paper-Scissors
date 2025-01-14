7255e3cf66f966bf03df581298eca7226839ecc2
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2025-01-14 09:13:10 -0500

    Beta Game works in console

--- classic.js
+++ classic.js
@@ -1,44 +1,84 @@
+window.onload = getName();
 // Generates random number if guest name Chosen
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  let randomNumber = getRandomNumber(0, 9999);
+}
+
 
 //inputs username
-let userName = prompt('What is your name:')
-userName ? document.getElementById('name').innerText = `${userName}`:
-document.getElementById('name').innerText =`Guest_` + `${randomNumber}`
+function getName() {
+  let randomNumber = getRandomNumber(0, 9999);
+  let userName = prompt('What is your name:')
+  userName ? document.getElementById('name').innerText = `${userName}`:
+  document.getElementById('name').innerText =`Guest_` + `${randomNumber}`
+}
+
 
 //opponent choice
 function opponentChoice(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-let computerNumber = opponentChoice(1, 3);
 
 // assigns opponent choice a proper choice in RPS
-if(computerNumber === 1){
- let opponentChoice = 'rock'
-}
-else if(computerNumber === 2){
-  let opponentChoice = 'paper'
-}
-else {
-  let opponentChoice = 'scissors'
+let computerChoice;
+function computerResponse() {
+  let computerNumber = opponentChoice(1, 3);
+  if (computerNumber === 1){
+    computerChoice = 'rock'
+   }
+   else if(computerNumber === 2){
+     computerChoice = 'paper'
+   }
+   else {
+     computerChoice = 'scissors'
+   }
+   console.log(computerChoice)
 }
 
+
 //If rock is chosen
+let playerChoice;
 function rock(){
-let playerChoice = 'rock'
+  playerChoice = 'rock'
+  computerResponse()
+  determine()
 }
 
 //if paper is chosen
 function paper(){
-let playerChoice = 'paper'
+  playerChoice = 'paper'
+  computerResponse()
+  determine()
 }
 
 //if scissors is chosen
 function scissors(){
-let playerChoice = 'scissors'
+  playerChoice = 'scissors'
+  computerResponse()
+  determine()
 }
 
-document.getElementById('results').innerText =`opponent choice: ` + `${opponentChoice}`
+//determine winner
+function determine(){
+  if(playerChoice === 'rock' && computerChoice === 'rock'){
+console.log('Tie')
+  } else if (playerChoice === 'rock' && computerChoice === 'paper'){
+console.log('Lose')
+  } else if (playerChoice === 'rock' && computerChoice === 'scissors'){
+console.log('Winner')
+  } else if (playerChoice === 'scissors' && computerChoice === 'rock'){
+console.log('Lose')
+  } else if (playerChoice === 'scissors' && computerChoice === 'paper'){
+console.log('winner')
+  } else if (playerChoice === 'scissors' && computerChoice === 'scissors'){
+console.log('Tie')
+  } else if (playerChoice === 'paper' && computerChoice === 'rock'){
+console.log('Winner')
+  } else if (playerChoice === 'paper' && computerChoice === 'paper'){
+console.log('Tie')
+  } else if (playerChoice === 'paper' && computerChoice === 'scissors'){
+console.log('Lose')
+  } else {
+    console.log('ERROR: HOW DID YOU MANAGE THIS?!')
+  }
+}
