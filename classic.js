da1e112c5492e39ac5d4a9cb8fcbf18217312779
Nvlahogiannis12 <526Nvlahogiannis@frhsd.com>
2025-01-24 09:28:16 -0500

    words change

--- classic.js
+++ classic.js
@@ -22,16 +22,19 @@ let computerChoice;
 function computerResponse() {
   let computerNumber = opponentChoice(1, 3);
   if (computerNumber === 1){
-    computerChoice = 'rock'
+    computerChoice = 'Rock'
     document.getElementById("opponent-box").src = "imgs/rock-RPS.png";
+    document.getElementById("computerResult").innerText = `Computer Chose: ${computerChoice}`
    }
    else if(computerNumber === 2){
-     computerChoice = 'paper'
+     computerChoice = 'Paper'
      document.getElementById("opponent-box").src = "imgs/paper-rps.png";
+     document.getElementById("computerResult").innerText = `Computer Chose: ${computerChoice}`
    }
    else {
-     computerChoice = 'scissors'
+     computerChoice = 'Scissors'
      document.getElementById("opponent-box").src = "imgs/scissors-RPS.png";
+     document.getElementById("computerResult").innerText = `Computer Chose: ${computerChoice}`
    }
    console.log(computerChoice)
 }
@@ -39,49 +42,54 @@ function computerResponse() {
 //If rock is chosen
 let playerChoice;
 function rock(){
-  playerChoice = 'rock'
+  playerChoice = 'Rock'
   document.getElementById("player-box").src = "imgs/rock-RPS.png";
   computerResponse()
   determine()
+    document.getElementById("playerResult").innerText = `Player Chose: ${playerChoice}`
 }
 
 //if paper is chosen
 function paper(){
-  playerChoice = 'paper'
+  playerChoice = 'Paper'
   document.getElementById("player-box").src = "imgs/paper-rps.png";
   computerResponse()
   determine()
+    document.getElementById("playerResult").innerText = `Player Chose: ${playerChoice}`
 }
 
 //if scissors is chosen
 function scissors(){
-  playerChoice = 'scissors'
+  playerChoice = 'Scissors'
   document.getElementById("player-box").src = "imgs/scissors-RPS.png";
   computerResponse()
   determine()
+    document.getElementById("playerResult").innerText = `Player Chose: ${playerChoice}`
 }
 
 //determine winner
 function determine(){
-  if(playerChoice === 'rock' && computerChoice === 'rock'){
+  if(playerChoice === 'Rock' && computerChoice === 'Rock'){
 console.log('Tie')
-  } else if (playerChoice === 'rock' && computerChoice === 'paper'){
+  } else if (playerChoice === 'Rock' && computerChoice === 'Paper'){
 console.log('Lose')
-  } else if (playerChoice === 'rock' && computerChoice === 'scissors'){
+  } else if (playerChoice === 'Rock' && computerChoice === 'Scissors'){
 console.log('Winner')
-  } else if (playerChoice === 'scissors' && computerChoice === 'rock'){
+  } else if (playerChoice === 'Scissors' && computerChoice === 'Rock'){
 console.log('Lose')
-  } else if (playerChoice === 'scissors' && computerChoice === 'paper'){
+  } else if (playerChoice === 'Scissors' && computerChoice === 'Paper'){
 console.log('winner')
-  } else if (playerChoice === 'scissors' && computerChoice === 'scissors'){
+  } else if (playerChoice === 'Scissors' && computerChoice === 'Scissors'){
 console.log('Tie')
-  } else if (playerChoice === 'paper' && computerChoice === 'rock'){
+  } else if (playerChoice === 'Paper' && computerChoice === 'Rock'){
 console.log('Winner')
-  } else if (playerChoice === 'paper' && computerChoice === 'paper'){
+  } else if (playerChoice === 'Paper' && computerChoice === 'Paper'){
 console.log('Tie')
-  } else if (playerChoice === 'paper' && computerChoice === 'scissors'){
+  } else if (playerChoice === 'Paper' && computerChoice === 'Scissors'){
 console.log('Lose')
   } else {
     console.log('ERROR: HOW DID YOU MANAGE THIS?!')
   }
 }
+
+//word display of chosen object
